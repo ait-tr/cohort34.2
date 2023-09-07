21344a0ee0bc580dac6c35b369ba0e8779d5d4a7
AR1988 <37342319+AR1988@users.noreply.github.com>
2023-09-07 13:48:16 +0200

    update less 02

--- basic_programming/lesson_02/code/hello_world/src/Dry.java
+++ basic_programming/lesson_02/code/hello_world/src/Dry.java
@@ -1,32 +1,59 @@
-
 public class Dry {
 
     public static void main(String... arg) {
-        greetAndrej();
+
+        greet();
         // System.out.println("Привет, меня зовут Андрей");
-        // meetMaria();
-        System.out.println("Привет, меня зовут Маша");
-        System.out.println("Приятно познакомиться, хорошего вечера");
+        meetMaria();
+        // System.out.println("Привет, меня зовут Маша");
+        sayGoodBye();
+        // System.out.println("Приятно познакомиться, хорошего вечера");
 
-        greetAndrej();
+        greet();
         // System.out.println("Привет, меня зовут Андрей");
-        System.out.println("Привет, меня зовут Костя");
-        System.out.println("Приятно познакомиться, хорошего вечера");
+        meetKostja();
+        // System.out.println("Привет, меня зовут Костя");
+        sayGoodBye();
+        // System.out.println("Приятно познакомиться, хорошего вечера");
 
-        System.out.println("Привет, меня зовут Андрей");
-        System.out.println("Привет, меня зовут Настя");
-        System.out.println("Приятно познакомиться, хорошего вечера");
+        greet();
+        // System.out.println("Привет, меня зовут Андрей");
+        meetNastja();
+        // System.out.println("Привет, меня зовут Настя");
+        sayGoodBye();
+        // System.out.println("Приятно познакомиться, хорошего вечера");
 
-        System.out.println("Привет, меня зовут Андрей");
-        System.out.println("Привет, меня зовут Андрей");
-        System.out.println("Приятно познакомиться, хорошего вечера");
+        greet();
+        // System.out.println("Привет, меня зовут Андрей");
+        meetAndrej();
+        // System.out.println("Привет, меня зовут Андрей");
+        sayGoodBye();
+        // System.out.println("Приятно познакомиться, хорошего вечера");
     }
 
-    public static void greetAndrej() {
+    public static void greet() {
         System.out.println("Привет, меня зовут Андрей");
     }
 
     public static void meetMaria() {
+        System.out.println("Привет, меня зовут Маша");
+    }
 
+    public static void meetKostja() {
+        System.out.println("Привет, меня зовут Костя");
+    }
+
+
+    public static void meetNastja() {
+        System.out.println("Привет, меня зовут Настя");
+    }
+
+
+    public static void meetAndrej() {
+        System.out.println("Привет, меня зовут Андрей");
+    }
+
+    public static void sayGoodBye() {
+        System.out.println("Приятно познакомиться, хорошего вечера");
     }
 }
