ff7e1d3bcbe8af704bca4ddf032de32cbf9282e2
AR1988 <37342319+AR1988@users.noreply.github.com>
2023-09-19 08:54:02 +0200

    update l11

--- basic_programming/lesson_11/code/HwSolution_10/src/DepositWhileLoop.java
+++ basic_programming/lesson_11/code/HwSolution_10/src/DepositWhileLoop.java
@@ -6,16 +6,112 @@
 public class DepositWhileLoop {
     public static void main(String[] args) {
         float initialDeposit = 100;
-        int months = 1;
+        int months = 10;
         float interestRate = 12;
         int currentMonth = 1;
 
+        calculateLoop(initialDeposit, months, interestRate, currentMonth);
+        System.out.print("\n-----------------------------------------------\n\n");
+        calculateWithoutLoop(initialDeposit, months, interestRate, currentMonth);
+    }
+
+    public static void calculateLoop(float initialDeposit,
+                                     int months,
+                                     float interestRate,
+                                     int currentMonth) {
+
         while (currentMonth <= months) {
-            initialDeposit += initialDeposit * (interestRate / 100);
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
             currentMonth++;
         }
 
-        System.out.println("Final deposit after 1 month: " + initialDeposit);
+        System.out.println("Final deposit after " + months + " month: " + initialDeposit);
+    }
+
+    public static void calculateWithoutLoop(float initialDeposit,
+                                            int months,
+                                            float interestRate,
+                                            int currentMonth) {
+
+        // 1 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        // 2 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        // 3 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        // 4 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        // 5 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        // 6 месяц
+        if (currentMonth <= months) {
+            System.out.println(currentMonth + " месяц");
+
+            float summ = initialDeposit * (interestRate / 100);
+            System.out.println("Сумма начисления " + summ);
+
+            initialDeposit += summ;
+            System.out.println("Текущий депозит " + initialDeposit);
+            currentMonth++;
+        }
+
+        System.out.println("Final deposit after " + months + " month: " + initialDeposit);
     }
 }
 
--- basic_programming/lesson_11/code/HwSolution_10/src/PopulationGrowth.java
+++ basic_programming/lesson_11/code/HwSolution_10/src/PopulationGrowth.java
@@ -1,22 +1,122 @@
 /**
  * @author Andrej Reutow
  * created on 17.09.2023
- *  Расчет численности населения
+ * Расчет численности населения
  */
 public class PopulationGrowth {
     public static void main(String[] args) {
         int population = 14_000_000;
         int birthRate = 14;
         int deathRate = 8;
-        int years = 10;
+        int years = 5;
 
+
+        populationCalcLoop(population, birthRate,deathRate,years);
+        System.out.println("Without loop");
+        populationCalcWithoutLoop(population, birthRate,deathRate,years);
+    }
+    public static void populationCalcLoop(int population, int birthRate, int deathRate, int years){
         for (int year = 1; year <= years; year++) {
             int births = (population * birthRate) / 1000;
             int deaths = (population * deathRate) / 1000;
-            population += (births - deaths);
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + year);
+            System.out.println("Рождаемость " + births + " в " + year + " году");
+            System.out.println("Смертность " + deaths + " в " + year + " году");
+            System.out.println("Прирост " + grow + " в " + year + " году");
+            System.out.println("Население " + population + " в " + year + " году");
+            System.out.println();
         }
 
-        System.out.println("Population after 10 years: " + population);
+        System.out.println("Population after " + years + " years: " + population);
+    }
+    public static void populationCalcWithoutLoop(int population, int birthRate, int deathRate, int years) {
+        int currentYear = 1;
+        // 1 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
+        // 2 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
+        // 3 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
+        // 4 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
+        // 5 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
+        // 6 год
+        if (currentYear <= years) {
+            int births = (population * birthRate) / 1000;
+            int deaths = (population * deathRate) / 1000;
+            int grow = (births - deaths);
+            population += grow;
+            System.out.println("Год " + currentYear);
+            System.out.println("Рождаемость " + births + " в " + currentYear + " году");
+            System.out.println("Смертность " + deaths + " в " + currentYear + " году");
+            System.out.println("Прирост " + grow + " в " + currentYear + " году");
+            System.out.println("Население " + population + " в " + currentYear + " году");
+            System.out.println();
+            currentYear++;
+        }
     }
 }
 
