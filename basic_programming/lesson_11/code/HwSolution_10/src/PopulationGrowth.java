575d32417e513b789a50f2de3c8d1fd19aac8e1b
AR1988 <37342319+AR1988@users.noreply.github.com>
2023-09-19 10:11:18 +0200

    update l11

--- basic_programming/lesson_11/code/HwSolution_10/src/PopulationGrowth.java
+++ basic_programming/lesson_11/code/HwSolution_10/src/PopulationGrowth.java
@@ -11,11 +11,12 @@ public class PopulationGrowth {
         int years = 5;
 
 
-        populationCalcLoop(population, birthRate,deathRate,years);
+        populationCalcLoop(population, birthRate, deathRate, years);
         System.out.println("Without loop");
-        populationCalcWithoutLoop(population, birthRate,deathRate,years);
+        populationCalcWithoutLoop(population, birthRate, deathRate, years);
     }
-    public static void populationCalcLoop(int population, int birthRate, int deathRate, int years){
+
+    public static void populationCalcLoop(int population, int birthRate, int deathRate, int years) {
         for (int year = 1; year <= years; year++) {
             int births = (population * birthRate) / 1000;
             int deaths = (population * deathRate) / 1000;
@@ -31,14 +32,15 @@ public class PopulationGrowth {
 
         System.out.println("Population after " + years + " years: " + population);
     }
+
     public static void populationCalcWithoutLoop(int population, int birthRate, int deathRate, int years) {
         int currentYear = 1;
         // 1 год
         if (currentYear <= years) {
-            int births = (population * birthRate) / 1000;
-            int deaths = (population * deathRate) / 1000;
-            int grow = (births - deaths);
-            population += grow;
+            int births = (population * birthRate) / 1000; // 196_000 ((14_000_000 * 14) / 1000)
+            int deaths = (population * deathRate) / 1000; // 11_2000 ((14_000_000 * 8) / 1000)
+            int grow = (births - deaths); // 84_000
+            population += grow; // 14_084_000 (14_000_000 + 84_000)
             System.out.println("Год " + currentYear);
             System.out.println("Рождаемость " + births + " в " + currentYear + " году");
             System.out.println("Смертность " + deaths + " в " + currentYear + " году");
@@ -49,10 +51,11 @@ public class PopulationGrowth {
         }
         // 2 год
         if (currentYear <= years) {
-            int births = (population * birthRate) / 1000;
-            int deaths = (population * deathRate) / 1000;
-            int grow = (births - deaths);
-            population += grow;
+            // population 14_084_000
+            int births = (population * birthRate) / 1000; // 197_176 ((14_084_000 * 14) / 1000)
+            int deaths = (population * deathRate) / 1000; // 112_672 ((14_084_000 * 8) / 1000)
+            int grow = (births - deaths); // 84_504 (197_176 - 112_672)
+            population += grow; // 14_168_504 (14_084_000 + 84_504)
             System.out.println("Год " + currentYear);
             System.out.println("Рождаемость " + births + " в " + currentYear + " году");
             System.out.println("Смертность " + deaths + " в " + currentYear + " году");
