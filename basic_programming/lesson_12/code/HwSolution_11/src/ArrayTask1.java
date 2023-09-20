dc78176e75714cffa45d6bc898cf39afa9a63b9c
AR1988 <37342319+AR1988@users.noreply.github.com>
2023-09-20 10:19:37 +0200

    update l12

--- basic_programming/lesson_12/code/HwSolution_11/src/ArrayTask1.java
+++ basic_programming/lesson_12/code/HwSolution_11/src/ArrayTask1.java
@@ -9,12 +9,14 @@ public class ArrayTask1 {
 
     public static void main(String[] args) {
         int[] mas = {15, -1, 5, 8, 10, -13, -9, 8, 7, 10, -2,  -3};
-        int maxNumber = -16;
         int maxIndex = 0;
+//        int maxNumber = mas[0];
+        int maxNumber = mas[maxIndex]; // 15
 
         for (int i = 0; i < mas.length; i++) {
-            mas[i] = (int) (Math.random() * 31) - 15;
-            if (maxNumber <= mas[i]) {
+            if (maxNumber <= mas[i]) {  // i = 0, maxNumber = 15, mas[i] = -1 (15 <= 15)
+                                        // i = 1, maxNumber = 15, mas[i] = -1 (15 <= -1)
+                                        // i = 2, maxNumber = 15, mas[i] = 5 (15 <= 5)
                 maxNumber = mas[i];
                 maxIndex = i;
             }
