4a62719eca0dd1e9dcfea57079cbb4bf0e92ad55
AR1988 <37342319+AR1988@users.noreply.github.com>
2023-10-10 09:11:57 +0200

    u l25

--- basic_programming/lesson_25/code/PasswordValidator_25/src/PasswordValidator.java
+++ basic_programming/lesson_25/code/PasswordValidator_25/src/PasswordValidator.java
@@ -29,12 +29,7 @@ public class PasswordValidator {
                              int minLength,
                              String symbolList,
                              int minSymbolCount) {
-        this.minLowerCase = minLowerCase;
-        this.minUpperCase = minUpperCase;
-        this.minDigits = minDigits;
-        this.minLength = minLength;
-        this.symbolList = symbolList;
-        this.minSymbolCount = minSymbolCount;
+
     }
 
     /**
@@ -45,11 +40,7 @@ public class PasswordValidator {
      */
     public boolean isValid(String password) {
 
-        return isLengthValid &&
-                isUpperCaseContaints &&
-                isLowerCaseContaints &&
-                isSymbolsContaints &&
-                isDigisContaints;
+        return false;
     }
 
     /**
@@ -59,15 +50,7 @@ public class PasswordValidator {
      * @return true, если пароль содержит заданное количество цифр, и false в противном случае.
      */
     private boolean isDigitsContains(String password) {
-        int digisCounter = 0;
-        for (int i = 0; i < password.length(); i++) {
-            if (Character.isDigit(password.charAt(i))) {
-                digisCounter++;
-                if (digisCounter == this.minSymbolCount) {
-                    return true;
-                }
-            }
-        }
+
         return false;
     }
 
@@ -78,15 +61,7 @@ public class PasswordValidator {
      * @return true, если пароль содержит заданное количество символов из списка, и false в противном случае.
      */
     private boolean isSymbolsContains(String password) {
-        int symbolCounter = 0;
-        for (int i = 0; i < password.length(); i++) {
-            if (symbolList.indexOf(password.charAt(i)) >= 0) {
-                symbolCounter++;
-                if (symbolCounter == this.minSymbolCount) {
-                    return true;
-                }
-            }
-        }
+
         return false;
     }
 
@@ -97,15 +72,7 @@ public class PasswordValidator {
      * @return true, если пароль содержит заданное количество символов верхнего регистра, и false в противном случае.
      */
     private boolean isUpperCaseContains(String password) {
-        int upperCaseCounter = 0;
-        for (int i = 0; i < password.length(); i++) {
-            if (Character.isUpperCase(password.charAt(i))) {
-                upperCaseCounter++;
-                if (upperCaseCounter == this.minUpperCase) {
-                    return true;
-                }
-            }
-        }
+
         return false;
     }
 
@@ -116,15 +83,7 @@ public class PasswordValidator {
      * @return true, если пароль содержит заданное количество символов нижнего регистра, и false в противном случае.
      */
     private boolean isLowerCaseContains(String password) {
-        int lowerCaseCounter = 0;
-        for (int i = 0; i < password.length(); i++) {
-            if (Character.isLowerCase(password.charAt(i))) {
-                lowerCaseCounter++;
-                if (lowerCaseCounter == this.minLowerCase) {
-                    return true;
-                }
-            }
-        }
+
         return false;
     }
 
@@ -135,6 +94,6 @@ public class PasswordValidator {
      * @return true, если длина пароля больше или равна минимальной длине, и false в противном случае.
      */
     private boolean isLengthValid(String password) {
-        return password.length() >= this.minLength;
+        return false;
     }
 }
