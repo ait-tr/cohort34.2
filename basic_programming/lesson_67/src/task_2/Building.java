d2afcc68a688e9dc750a1b19ba53f884b04cf2c3
Andrey <k-udm@ya.ru>
2023-12-15 13:04:48 +0100

    Lesson 67

---
+++ basic_programming/lesson_67/src/task_2/Building.java
@@ -0,0 +1,39 @@
+package task_2;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Building implements Runnable {
+
+    private String city;
+    private String[] stages;
+    private boolean printTime;
+
+    public Building(String city, String[] stages, boolean printTime) {
+        this.city = city;
+        this.stages = stages;
+        this.printTime = printTime;
+    }
+
+    @Override
+    public void run() {
+        for (String stage : stages) {
+            if (printTime) {
+                // Berlin: 1. Проектная деятельность, Thread_name -> time
+                System.out.printf("%s: %s, %s -> %s%n",
+                        city, stage, Thread.currentThread().getName(), new Date());
+            } else {
+                System.out.printf("%s: %s, %s%n",
+                        city, stage, Thread.currentThread().getName());
+            }
+
+            Random random = new Random();
+            int delay = random.nextInt(4000) + 1000;
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ basic_programming/lesson_67/theory.md
@@ -0,0 +1,1 @@
+https://raw.githubusercontent.com/ait-tr/cohort34.2/main/basic_programming/lesson_67/resources/67_ThreadExecutors.pptx
