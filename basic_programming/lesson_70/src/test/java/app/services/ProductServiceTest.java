379b028a430417e4c2daa839cdf17401a61d8a7e
Andrey <k-udm@ya.ru>
2023-12-22 12:59:25 +0100

    Lesson 71

--- basic_programming/lesson_70/src/test/java/app/services/ProductServiceTest.java
+++ basic_programming/lesson_70/src/test/java/app/services/ProductServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.Mockito;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 class ProductServiceTest {
 
@@ -33,11 +35,65 @@ class ProductServiceTest {
 
     @Test
     public void checkIfSupplierInfoWasDeleted() {
-        Mockito.when(merchService.withoutSupplierInfo()).thenReturn(true);
+        when(merchService.withoutSupplierInfo()).thenReturn(true);
         List<Product> products = service.getAll();
 
         for (Product product : products) {
             assertNull(product.getSupplier());
         }
+    }
+
+    @Test
+    public void checkIfSupplierInfoWasNotDeleted() {
+        List<Product> products = service.getAll();
+
+        for (Product product : products) {
+            assertNotNull(product.getSupplier());
+        }
+    }
+
+    @Test
+    public void checkCorrectMarkup() {
+        when(merchService.getMarkup()).thenReturn(30.0);
+        Product product = service.getById(1);
+        double expected = 156;
+        double actual = product.getPrice();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkIfCorrectArticleWasSet() {
+        Product product = new Product("test", 1, "test");
+        String testArticle = "Test777";
+
+        doAnswer(x -> {
+            Product product1 = x.getArgument(0);
+            product1.setArticle(testArticle);
+            return product1;
+        }).when(merchService).setArticle(product);
+
+        service.save(product);
+        product = service.getById(4);
+
+        assertNotNull(product);
+        assertEquals(testArticle, product.getArticle());
+    }
+
+    @Test
+    public void checkIfInactiveStatusWasSet() {
+        service.deleteById(2);
+        Product product = service.getById(2);
+
+        assertNotNull(product);
+        assertFalse(product.isActive());
+    }
+
+    @Test
+    public void checkFullDeletion() {
+        when(merchService.fullDeletion()).thenReturn(true);
+        service.deleteById(2);
+        Product product = service.getById(2);
+
+        assertNull(product);
     }
 }
