fa24db0603407862f809be66852b5dfa77265391
v1148656 <v1148656@gmail.com>
2024-02-05 14:21:39 +0200

    Add lesson_21

---
+++ front_end/lesson_21/code/21_react-app_05_02_2024/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './components/Counter';
+// import { CounterClass } from './components/CounterClass';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      {/* <CounterClass /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end/lesson_21/code/21_react-app_05_02_2024/src/components/Counter.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    // React.useState(0)
+    const [count, setCount] = useState(0);
+    
+    // console.log(count);
+    // setCount(1); меняется значение локального состояния (count) с
+    // помощью специального метода setCount()
+
+    // let x = 0;
+    // console.log(x--);
+    // x--; // x = x - 1
+
+    const increment = () => {
+        // console.log(e);
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h2>Counter: {count}</h2>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={increment}>Increment</button>
+        </div>
+    )
+}
+
+export default Counter
